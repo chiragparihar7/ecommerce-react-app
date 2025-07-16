91a2ec9b409a003a74506c460122eb996f5fa465
chiragparihar7 <chiragparihar118@gmail.com>
2025-07-16 12:09:27 +0530

    Implement login and registration forms with validation using Formik and Yup

---
+++ src/containers/seller/auth/Login.jsx
@@ -0,0 +1,110 @@
+import { useFormik } from "formik";
+import React from "react";
+import * as Yup from "yup";
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      passord: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      console.log("Login with values:", values);
+
+      // Login wiht fake API call
+      try {
+        const fakeSeller = {
+          email: "fakesellaer123@gmail.com",
+          password: "fakepassword",
+        };
+
+        if (
+          values.email === fakeSeller.email &&
+          values.password === fakeSeller.password
+        ) {
+          alert("Login Successful!");
+          // Store token, redirect to seller dashboard, etc.
+          localStorage.setItem("authToken", "fakeToken123");
+        } else {
+          alert("Invalid cendientials, please try again.");
+        }
+      } catch (error) {
+        alert("Something went wrong, please try again later.");
+        console.error("Login error:", error);
+      }
+    },
+  });
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center mb-6 text-gray-80 ">
+          Seller Login
+        </h2>
+        <form onSubmit={formik.handleSubmit} className="space-y-5">
+          {/* { email } */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              className={`w-full py-2 px-2 border rounded-md focus:outline-none ${
+                formik.touched.email && formik.errors.email
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              } `}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <p className="text-red-500 text-sm mt-1">{formik.errors.email}</p>
+            )}
+          </div>
+          {/* { password } */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                formik.touched.password && formik.errors.password
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              }`}
+            />
+            {formik.touched.password && formik.errors.password && (
+                <p className="text-red-500 text-sm mt-1">{formik.errors.password}</p>
+            )}
+          </div>
+          {/* Submit Button */}
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-200"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/containers/seller/auth/Register.jsx
@@ -0,0 +1,141 @@
+import { useFormik } from "formik";
+import React from "react";
+import * as Yup from "yup";
+
+function Register() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("nameis required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is rquired"),
+      password: Yup.string()
+        .min(6, "password must be at least 6 characters")
+        .required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Confim Password your passord"),
+    }),
+    onSubmit: (values) => {
+      console.log("From Submitted", values);
+      alert("Form Submitted Successfully!");
+    },
+  });
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
+          Seller Register
+        </h2>
+        <form onSubmit={formik.handleSubmit} className="space-y-5">
+          {/* name */}
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
+              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                formik.touched.name && formik.errors.name
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              }`}
+            />
+            {formik.touched.name && formik.errors.name && (
+              <p className="text-red-500 text-sm mt-1">{formik.errors.name}</p>
+            )}
+          </div>
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                formik.touched.email && formik.errors.email
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              }`}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <p className="text-red-500 text-sm mt-1">{formik.errors.email}</p>
+            )}
+          </div>
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                formik.touched.password && formik.errors.password
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              }`}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <p className="text-red-500 text-sm mt-1">
+                {formik.errors.password}
+              </p>
+            )}
+          </div>
+          {/* Confirm Password */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.confirmPassword}
+              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+                  ? "border-red-500"
+                  : "border-gray-300 focus:ring-2 focus:ring-blue-500"
+              }`}
+            />
+            {formik.touched.confirmPassword &&
+              formik.errors.confirmPassword && (
+                <p className="text-red-500 text-sm mt-1">
+                  {formik.errors.confirmPassword}
+                </p>
+              )}
+          </div>
+          {/* Submit */}
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-md font-semibold hover:bg-blue-700 transition"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
