ee68d6d599688e526b1a8cf124e744899ba246d5
sunilthakor143 <sjthakor6787@gmail.com>
2025-07-17 16:47:11 +0530

    create a Home component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,61 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Login from './user/auth/login/Login';
 // import Register from './user/auth/register/Register';
 import Header from './user/layout/Header.jsx';
 import HeroSection from './user/layout/Hero-Section.jsx';
 import Footer from './user/layout/Footer.jsx';
+import Home from './user/layout/Home.jsx';
 
-const Home = () => (
-  <>
-    <Header />
-    <HeroSection />
-    <Footer />
-  </>
-);
-=======
-// import Login from './containers/admin/auth/login/Login'
-import SellerLogin from './containers/seller/auth/Login';
-import SellerRegister from './containers/seller/auth/Register'; 
-import AdminLogin from './containers/admin/auth/login/Login'
-import AdminDashboard from './containers/admin/pages/dashboard/Dashboard'
-import Register from './user/auth/register/Register';
-import Login from './user/auth/login/Login';
->>>>>>> 76309f07feeeaf99aa073b418cf46b9cb6eba977
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
+        {/* User routes */}
+        {/* <Route path="/" element={<Login />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
 
-        {/* user route  */}
-        {/* <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> */}
         <Route path="/" element={<Home />} />
 
-
-        {/* admin route  */}
-
-=======
-      
-        {/* Seller Routes */}
-        <Route path="/seller/register" element={<SellerRegister/>} />
-        <Route path="/seller/login" element={<SellerLogin/>} />
-
-
-
-        {/* Admin Routes */}
-        <Route path="/admin/login" element={<AdminLogin/>} />
-        <Route path='/admin/dashboard' element={<AdminDashboard />} />
-        
-
-
-        {/* User Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path='/register' element={<Register />} />
->>>>>>> 76309f07feeeaf99aa073b418cf46b9cb6eba977
+        {/* Admin routes */}
+        {/* Add admin routes here */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/user/layout/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./Hero-Section";
+
+const Home = () => (
+  <>
+    <Header />
+    <HeroSection />
+    <Footer />
+  </>
+);
+
+
+export default Home;
