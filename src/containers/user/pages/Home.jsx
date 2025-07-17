7a7be044a505b6022283d091b984bfe68c6b1a86
sunilthakor143 <sjthakor6787@gmail.com>
2025-07-17 18:03:59 +0530

    change folder structure

---
+++ src/containers/user/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from '../layout/Footer';
+import Header from "../layout/Header";
+import HeroSection from "../layout/HeroSection";
+
+
+
+const Home = () => (
+  <>
+    <Header />
+    <HeroSection />
+    <Footer />
+  </>
+);
+
+export default Home;
