ae757e5b4120b3445c992e9de909e9747a8a3a7e
chiragparihar7 <chiragparihar118@gmail.com>
2025-07-17 10:41:26 +0530

    Update dependencies and refactor routing for login and registration flow

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './containers/admin/auth/login/Login'
+// import Login from './containers/admin/auth/login/Login'
+import Login from './containers/seller/auth/Login';
+import Register from './containers/seller/auth/Register'; 
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+      
+        <Route path="/" element={<Register/>} />
+        <Route path="/Login" element={<Login/>} />
+
       </Routes>
     </BrowserRouter>
   );
