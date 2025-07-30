0fa92d3bba0214a4982175123f88b3b1e07389db
sunilthakor143 <sjthakor6787@gmail.com>
2025-07-30 17:44:11 +0530

    add products in cart

--- src/config/DataService.js
+++ src/config/DataService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://192.168.1.105:5000/api";
-// const API_BASE_URL = "http://192.168.1.105:5000/api";
 
 const DataService = (token = '') => {
   return axios.create({
